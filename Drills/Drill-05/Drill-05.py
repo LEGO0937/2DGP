ff17a768d60aae02a67d3618a85d693a2d162de8
ParkJM(NoteBook) <39212289+LEGO0937@users.noreply.github.com>
2018-09-17 17:00:51 +0900

    Drill-05 점에서 점으로 가는 함수 선언
    2015180014 박정만
    점 1부터 10까지 가는 함수를 총 10개 구현하였다

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -3,81 +3,41 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
-def move_from_center_to_right():
-    x, y = 800 // 2, 90
-    while x < 800 - 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x+=2
-        delay(0.01)
-
-def move_up():
-    x, y = 800 - 25 // 2, 90
-    while y < 600 - 50:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y += 2
-        delay(0.01)
-
-def move_left():
-    x, y = 800 - 25 // 2, 600 - 50
-    while x > 0 + 25 // 2:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x -= 2
-        delay(0.01)
-
-def move_down():
-    x, y = 0 // 2, 600 - 50
-    while y > 0 + 90:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y -= 2
-        delay(0.01)
-
-def move_from_left_to_center():
-    x, y = 0 // 2, 0 + 90
-    while x < 400 - 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
-
-
-
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_left_to_center()
-
-import math
-
-def make_circle():
-    cx, cy, r = 800 // 2, 600 // 2, (600-180)//2
-    degree = -90
-    while degree < 270:
-        radian = math.radians(degree)
-        x = cx + r * math.cos(radian)
-        y = cy + r * math.sin(radian)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        degree += 1
-        delay(0.01)
+def go_point1_to_point2():
+    pass
+def go_point2_to_point3():
+    pass
+def go_point3_to_point4():
+    pass
+def go_point4_to_point5():
+    pass
+def go_point5_to_point6():
+    pass
+def go_point6_to_point7():
+    pass
+def go_point7_to_point8():
+    pass
+def go_point8_to_point9():
+    pass
+def go_point9_to_point10():
+    pass
+def go_point10_to_point1():
+    pass
 
 
 while True:
-    make_rectangle()
-    make_circle()
+    go_point1_to_point2()
+    go_point2_to_point3()
+    go_point3_to_point4()
+    go_point4_to_point5()
+    go_point5_to_point6()
+    go_point6_to_point7()
+    go_point7_to_point8()
+    go_point8_to_point9()
+    go_point9_to_point10()
+    go_point10_to_point1()
 
 #함수의 이름의 시작은 항상 동사(verb)로 한다
 
