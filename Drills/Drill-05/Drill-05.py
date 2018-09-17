6bede7052bd5a123a5374563f000b08b7c1ec939
ParkJM(NoteBook) <39212289+LEGO0937@users.noreply.github.com>
2018-09-17 16:21:35 +0900

    Drill-05 위로가는 함수완성
    2015180014 박정만
    캐릭터가 위로 올라가는 함수 완성

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -15,7 +15,13 @@ def move_from_center_to_right():
         delay(0.01)
 
 def move_up():
-    pass
+    x, y = 800 - 25 // 2, 90
+    while y < 600 - 50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
 
 def move_left():
     pass
@@ -29,7 +35,7 @@ def move_from_left_to_center():
 
 
 def make_rectangle():
-    move_from_center_to_right()
+    # move_from_center_to_right()
     move_up()
     move_left()
     move_down()
