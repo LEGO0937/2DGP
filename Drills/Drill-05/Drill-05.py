4fc3f094919b17cc6c7fb8e8fda866235f376193
ParkJM(NoteBook) <39212289+LEGO0937@users.noreply.github.com>
2018-09-17 15:56:36 +0900

    Drill-05 시작
    2015180014 박정만

---
+++ Drills/Drill-05/Drill-05.py
@@ -0,0 +1,18 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+
+#fill here
+
+
+
+#아랫줄로 개행할 때 Shift+Enter키를 누르면 문장 중간에서 바로 개행된다
+#코드 중간에 뜨는 회색줄은 프로그램이 돌아가는데 지장이 없는것들
+#코드 중간에 뜨는 빨간줄은 그냥 에러
+close_canvas()
+
+
