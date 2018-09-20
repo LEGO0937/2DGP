e47c13a0188df92180f12b8b8a3524fb73134f30
ParkJM(NoteBook) <39212289+LEGO0937@users.noreply.github.com>
2018-09-20 16:47:48 +0900

    Drill-06 마우스커서 구현
    마우스 커서를 구현하였으며 피벗을 검지에 맞추었다

--- Drills/Drill-06/Drill-06.py
+++ Drills/Drill-06/Drill-06.py
@@ -6,10 +6,13 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
     global running
     global x, y
+    global click_x, click_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            click_x, click_y = x, y
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, KPU_HEIGHT - 1 -  event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
@@ -21,15 +24,18 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
+
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
+click_x, click_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    mouse.draw(x + 26, y - 26)
     update_canvas()
     frame = (frame + 1) % 8
 
