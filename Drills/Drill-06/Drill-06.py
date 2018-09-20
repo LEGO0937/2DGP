b6c1ed2b9f0f6d9e62c0d5ff03a652663ff09db2
ParkJM(NoteBook) <39212289+LEGO0937@users.noreply.github.com>
2018-09-20 16:54:02 +0900

    Drill-06 캐릭터 좌표이동 함수 구현
    기존에 마우스커서랑 캐릭터랑 같이 이동하였지만 따로 함수를 선언하여 마우스는 마우스만, 캐릭터는 캐릭터만 움직일 수 있게 구현

--- Drills/Drill-06/Drill-06.py
+++ Drills/Drill-06/Drill-06.py
@@ -4,12 +4,23 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
 def go_to_mouse_point(clickX, clickY):
-    pass
+    global char_x, char_y
+    global plus_x, plus_y
+    global direction
+    plus_x, plus_y = clickX - char_x, clickY - char_y
+    if char_x > click_x:
+        direction = 0
+    elif char_x < click_x:
+        direction = 100
+    char_x += plus_x // 20
+    char_y += plus_y // 20
+
 
 def handle_events():
     global running
     global x, y
     global click_x, click_y
+    global stop_or_go
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -33,12 +44,16 @@ running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 click_x, click_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+char_x, char_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+plus_x, plus_y = 0, 0
+direction = 100
+stop_or_go = False
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, char_x, char_y)
     mouse.draw(x + 26, y - 26)
     update_canvas()
     frame = (frame + 1) % 8
