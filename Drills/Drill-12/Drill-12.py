1a75298d6ed3a1cc53950267286ff36f038f148d
ParkJM(NoteBook) <39212289+LEGO0937@users.noreply.github.com>
2018-10-29 20:11:11 +0900

    Drill-12 멈춰있는 주인공
    유체이탈을 하는동안 본체는 멈춰있어야하므로 주인공은 잠자는동안 프레임을 고정시켰다

--- Drills/Drill-12/Drill-12.py
+++ Drills/Drill-12/Drill-12.py
@@ -125,7 +125,7 @@ class SleepState:
     @staticmethod
     def draw(boy):
         if boy.dir == 1:
-            boy.image.clip_composite_draw(int(boy.frame) * 100, 300, 100, 100, 3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
+            boy.image.clip_composite_draw(100, 300, 100, 100, 3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
             if boy.round_timer < 1:
                 boy.ghost_image.opacify(0.1 * random.randint(1, 9))
                 boy.ghost_image.clip_composite_draw(100, 300, 100, 100, (3.141592 - (boy.round_timer * 3.141592)) / 2, '', boy.x - (25 - (boy.round_timer * 25)), boy.y - (25 - (boy.round_timer * 25)), 100, 100)
@@ -134,7 +134,7 @@ class SleepState:
                 boy.ghost_image.clip_draw(int(boy.frame) * 100, 300, 100, 100, boy.radian * math.cos(
                     math.radians(270 + (DEGREE_PER_SECOND * boy.round_timer))) + boy.x, 100 * math.sin(math.radians(270 + (DEGREE_PER_SECOND * boy.round_timer))) + boy.y * 2)
         else:
-            boy.image.clip_composite_draw(int(boy.frame) * 100, 200, 100, 100, -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
+            boy.image.clip_composite_draw(100, 200, 100, 100, -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
             if boy.round_timer < 1:
                 boy.ghost_image.opacify(0.1 * random.randint(1, 9))
                 boy.ghost_image.clip_composite_draw(100, 200, 100, 100, (-3.141592 + (boy.round_timer * 3.141592)) / 2, '', boy.x + (25 - (boy.round_timer * 25)), boy.y - (25 - (boy.round_timer * 25)), 100, 100)
