1486eb22faa8ae603ffbe45d738c25c3af88a7db
ParkJM(NoteBook) <39212289+LEGO0937@users.noreply.github.com>
2018-11-19 16:11:53 +0900

    Drill-13 boy객체 바로위에 좌표값 뜨게 수정
    boy객체 바로위에 좌표값이 나온다

--- Drills/Drill-13/boy.py
+++ Drills/Drill-13/boy.py
@@ -69,13 +69,15 @@ class WalkingState:
         boy.x += boy.x_velocity * game_framework.frame_time
         boy.y += boy.y_velocity * game_framework.frame_time
 
-        boy.x = clamp(boy.canvas_width // 2, boy.x, boy.bg.w - boy.canvas_width // 2)
-        boy.y = clamp(boy.canvas_height // 2, boy.y, boy.bg.h - boy.canvas_height // 2)
-
+        #boy.x = clamp(boy.canvas_width // 2, boy.x, boy.bg.w - boy.canvas_width // 2)
+        #boy.y = clamp(boy.canvas_height // 2, boy.y, boy.bg.h - boy.canvas_height // 2)
+        boy.x = clamp(0, boy.x, boy.bg.w)
+        boy.y = clamp(0, boy.y, boy.bg.h)
 
     @staticmethod
     def draw(boy):
-        cx, cy = boy.canvas_width // 2, boy.canvas_height // 2
+        #cx, cy = boy.canvas_width // 2, boy.canvas_height // 2
+        cx, cy = boy.x - boy.bg.window_left, boy.y - boy.bg.window_bottom
 
         if boy.x_velocity > 0:
             boy.image.clip_draw(int(boy.frame) * 100, 100, 100, 100, cx, cy)
@@ -142,7 +144,10 @@ class Boy:
 
     def draw(self):
         self.cur_state.draw(self)
-        self.font.draw(self.canvas_width//2 - 60, self.canvas_height//2 + 50, '(%5d, %5d)' % (self.x, self.y), (255, 255, 0))
+        #self.font.draw(self.canvas_width//2 - 60, self.canvas_height//2 + 50, '(%5d, %5d)' % (self.x, self.y), (255, 255, 0))
+        self.font.draw(self.x - self.bg.window_left - 60, self.y - self.bg.window_bottom + 50, '(%5d, %5d)' % (self.x, self.y),
+                       (255, 255, 0))
+
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
